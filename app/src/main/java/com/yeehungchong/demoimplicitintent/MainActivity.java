55df5dd18ac4225a5b5f088a6e59cc83a0f4ee6e
Yee Hung <19034609@myrp.edy.sg>
2021-04-29 12:27:11 +0800

    Initial commit

---
+++ app/src/main/java/com/yeehungchong/demoimplicitintent/MainActivity.java
@@ -0,0 +1,56 @@
+package com.yeehungchong.demoimplicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnEmail, btnRPwebsite;
+    EditText editTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextMessage = findViewById(R.id.editTextMessage);
+        btnEmail = findViewById(R.id.buttonEmail);
+        btnRPwebsite = findViewById(R.id.buttonRPwebsite);
+
+        btnEmail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                // The action you want this intent to do;
+                // ACTION_SEND is used to indicate sending text
+                Intent email = new Intent(Intent.ACTION_SEND);
+                // Put essentials like email address, subject & body text
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"jason_lim@rp.edu.sg"});
+                email.putExtra(Intent.EXTRA_SUBJECT,"Test Email from C347");
+                email.putExtra(Intent.EXTRA_TEXT, editTextMessage.getText());
+                // This MIME type indicates email
+                email.setType("message/rfc822");
+                // createChooser shows user a list of app that can handle
+                // this MIME type, which is, email
+                startActivity(Intent.createChooser(email,"Choose an Email client :"));
+            }
+        });
+
+        btnRPwebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // Intent to display data
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                // Set the URL to be used.
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
+
+    }
+}
